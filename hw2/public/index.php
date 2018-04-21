0a3629aa573d02417bdbd9c9b350b5b8bce34480
o.fadeev <o.fadeev@live.ru>
2018-04-21 21:50:59 +0300

    hw2

--- hw2/public/index.php
+++ hw2/public/index.php
@@ -3,7 +3,7 @@ include($_SERVER["DOCUMENT_ROOT"]."/../services/init.php");
 include($_SERVER["DOCUMENT_ROOT"]."/../services/Autoloader.php");
 spl_autoload_register([new \fadeev\php2\services\Autoloader(), 'loadClass']);
 
-$product = new \fadeev\php2\models\Product(123);
+$product = new \fadeev\php2\models\Product();
 echo "<pre>";
 var_dump($product);
 echo "</pre>";
