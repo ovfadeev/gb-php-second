a958820fd9bd4c08ae0ddff6c491064e1c86c556
o.fadeev <o.fadeev@live.ru>
2018-04-21 18:20:36 +0300

    hw1

--- hw1/index.php
+++ hw1/index.php
@@ -79,19 +79,4 @@ class UsersParams extends Users
     parent::__construct($name, $lastName, $email, $login, $password);
   }
 }
-
-class A {
-    public function foo() {
-        static $x = 0;
-        echo ++$x;
-    }
-}
-class B extends A {
-}
-$a1 = new A();
-$b1 = new B();
-$a1->foo();
-$b1->foo();
-$a1->foo();
-$b1->foo();
 ?>
