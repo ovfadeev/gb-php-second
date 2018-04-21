83873ce2308c1cd6f374e1e80bd795e1e8aaa531
o.fadeev <o.fadeev@live.ru>
2018-04-21 20:35:44 +0300

    hw2

--- hw2/models/DB.php
+++ hw2/models/DB.php
@@ -5,9 +5,36 @@ namespace fadeev\php2\models;
 */
 class DB
 {
-  function __construct()
+  private $dbName;
+  private $dbLogin;
+  private $dbPassword;
+  private $dbHost;
+  private $db;
+
+  function __construct($name, $login, $password, $host)
   {
-    # code...
+    $this->dbName = $name;
+    $this->dbLogin = $login;
+    $this->dbPassword = $password;
+    $this->dbHost = $host;
+  }
+
+  private function connect()
+  {
+    $this->db = mysqli_connect($this->dbHost, $this->dbLogin, $this->dbPassword, $this->dbName);
+    mysqli_query($this->db, "SET NAMES utf8");
+  }
+
+  private function close()
+  {
+    mysqli_close($this->db);
+  }
+
+  private function query($sql)
+  {
+    $result = mysqli_query($this->db, $sql);
+    $this->close();
+    return $result;
   }
 }
 ?>
--- hw2/services/Autoloader.php
+++ hw2/services/Autoloader.php
@@ -6,16 +6,17 @@ namespace fadeev\php2\services;
 class AutoLoader
 {
   const REMOVE_NAMESPACE = "fadeev\\php2\\";
+  const FILENAME = "/../#PATH#.php";
 
-  protected $path;
+  protected $filePath;
   protected $fileName;
 
   public function loadClass($className)
   {
     if (stristr($className, self::REMOVE_NAMESPACE))
     {
-      $this->path = str_replace(array(self::REMOVE_NAMESPACE, "\\"), array("", "/"), $className);
-      $this->fileName = str_replace("#PATH#", $this->path, $_SERVER["DOCUMENT_ROOT"]."/../#PATH#.php");
+      $this->filePath = str_replace(array(self::REMOVE_NAMESPACE, "\\"), array("", "/"), $className);
+      $this->fileName = str_replace("#PATH#", $this->filePath, $_SERVER["DOCUMENT_ROOT"].self::FILENAME);
     }
     if (file_exists($this->fileName))
     {
