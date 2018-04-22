b6447ed61a5ba7546ad3ec59c833c3b6d2746211
o.fadeev <o.fadeev@live.ru>
2018-04-22 07:13:26 +0300

    hw2

--- hw2/public/index.php
+++ hw2/public/index.php
@@ -1,7 +1,5 @@
 <?php
 include($_SERVER["DOCUMENT_ROOT"]."/../services/init.php");
-include($_SERVER["DOCUMENT_ROOT"]."/../services/Autoloader.php");
-spl_autoload_register([new \fadeev\php2\services\Autoloader(), 'loadClass']);
 
 $product = new \fadeev\php2\models\Product();
 echo "<pre>";
--- hw2/services/init.php
+++ hw2/services/init.php
@@ -5,4 +5,7 @@ define("DB_LOGIN", "root");
 define("DB_PASSWORD", "");
 define("DB_HOST", "localhost");
 define("DB_PREFIX_TABLE", "php2_");
+/* --- autoloader --- */
+include($_SERVER["DOCUMENT_ROOT"]."/../services/Autoloader.php");
+spl_autoload_register([new \fadeev\php2\services\Autoloader(), 'loadClass']);
 ?>
