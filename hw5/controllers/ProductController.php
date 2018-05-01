e8cfe6030dbab7f8fb8483a9311c8ed968e8641e
o.fadeev <o.fadeev@live.ru>
2018-05-01 16:03:01 +0300

    hw5

--- hw5/controllers/ProductController.php
+++ hw5/controllers/ProductController.php
@@ -6,17 +6,17 @@ use fadeev\php2\models\Product;
  */
 class ProductController extends Controller
 {
-    public function actionIndex()
-    {
-        $products = Product::GetList();
-        echo $this->render("products", array("products" => $products));
-    }
+  public function actionIndex()
+  {
+    $products = Product::GetList();
+    echo $this->render("products", array("products" => $products));
+  }
 
-    public function actionCard()
-    {
-       $id = htmlspecialchars($_GET["id"]);
-       $product = Product::GetById($id);
-       echo $this->render("card", array("product" => $product));
-    }
+  public function actionCard()
+  {
+    $id = htmlspecialchars($_GET["id"]);
+    $product = Product::GetById($id);
+    echo $this->render("card", array("product" => $product));
+  }
 }
 ?>
