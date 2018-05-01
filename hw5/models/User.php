ff5a9562fc3d09772de58f30fcfefdf576ad3871
o.fadeev <o.fadeev@live.ru>
2018-05-01 21:36:19 +0300

    hw5

--- hw5/models/User.php
+++ hw5/models/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace fadeev\php2\models;
 use fadeev\php2\models\DB;
+use fadeev\php2\models\UserAuth;
 /**
 * Пользователь
 */
@@ -51,43 +52,11 @@ class User extends DBModel
   }
 
   /**
-   * Шифруем пароль
-   * @param string $password
-   */
-  protected function HashPassword($password)
-  {
-    return password_hash($password, PASSWORD_DEFAULT);
-  }
-  /**
-   * Проверяем пароль
-   * @param string $password
-   */
-  protected function VerifyPassword($password, $db_password)
-  {
-    return password_verify($password, $db_password);
-  }
-
-  /**
-   * Авторизация
-   * @param string $login
-   * @param string $password
-   */
-  public static function Auth($login, $password)
-  {
-    $findUser = self::GetList(array("login" => $login), array("id", "password"))[0];
-    if (self::VerifyPassword($password, $findUser["password"]))
-    {
-      return self::GetById($findUser["id"]);
-    }
-    return false;
-  }
-
-  /**
    * Добавить пользователя
    */
   public function Add()
   {
-    $this->password = $this->HashPassword($this->password);
+    $this->password = UserAuth::HashPassword($this->password);
     parent::Add();
   }
 
