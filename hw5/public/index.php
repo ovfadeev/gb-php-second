6048a4c3f503adcd566a538b4ed330f2d5d0227c
o.fadeev <o.fadeev@live.ru>
2018-05-01 23:17:40 +0300

    hw5

--- hw5/public/index.php
+++ hw5/public/index.php
@@ -11,8 +11,8 @@ $actionName = $_GET['a'];
 $controllerClass = CONTROLLERS_NAMESPACE.ucfirst($controllerName)."Controller";
 
 if(class_exists($controllerClass)){
-  // $controller = new $controllerClass(new \fadeev\php2\services\TemplateRenderer());
-  $controller = new $controllerClass(new \fadeev\php2\services\TwigRenderer());
+  $controller = new $controllerClass(new \fadeev\php2\services\TemplateRenderer());
+  // $controller = new $controllerClass(new \fadeev\php2\services\TwigRenderer());
   $controller->RunAction($actionName);
 }
 ?>
