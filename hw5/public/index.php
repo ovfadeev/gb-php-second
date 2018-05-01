3bb99f8076aefcd0297b3d9540ee05739df6d736
o.fadeev <o.fadeev@live.ru>
2018-05-01 23:03:53 +0300

    hw5

--- hw5/public/index.php
+++ hw5/public/index.php
@@ -1,6 +1,11 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"]."/../config/main.php");
 
+// require_once(ROOT_DIR."vendor/autoload.php");
+// $loader = new Twig_Loader_Filesystem(TEMPLATES_DIR);
+// $twig = new Twig_Environment($loader);
+// echo $twig->render("index.php", array('name' => 'Fabien'));
+
 $controllerName = $_GET['c'] ?: 'index';
 $actionName = $_GET['a'];
 
@@ -8,7 +13,6 @@ $controllerClass = CONTROLLERS_NAMESPACE.ucfirst($controllerName)."Controller";
 
 if(class_exists($controllerClass)){
   $controller = new $controllerClass(new \fadeev\php2\services\TemplateRenderer());
-  // $controller = new $controllerClass(new \fadeev\php2\services\TwigRenderer());
   $controller->RunAction($actionName);
 }
 ?>
--- hw5/views/index.php
+++ hw5/views/index.php
@@ -1,1 +1,2 @@
 <h1><?="Main page"?></h1>
+<p>{{name}}</p>
