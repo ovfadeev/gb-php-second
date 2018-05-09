7827f5d011ae5c417490fded60f6c34a9456c20c
o.fadeev <o.fadeev@live.ru>
2018-05-09 15:58:03 +0300

    hw7

--- hw7/controllers/AuthController.php
+++ hw7/controllers/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
   public function actionLogout()
   {
     (new Sessions)->Remove("user_id");
-    header("Location: /auth/");
+    header("Location: /");
   }
 }
 ?>
