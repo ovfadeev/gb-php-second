89b6677f128de5bd07e611700fba0fb86c5c764f
o.fadeev <o.fadeev@live.ru>
2018-05-10 20:39:30 +0300

    hw7

--- hw7/config/main.php
+++ hw7/config/main.php
@@ -21,14 +21,7 @@ return array(
     ),
     "preparesql" => array(
       "class" => \fadeev\php2\services\Preparesql::class
-    ),
+    )
   )
 );
-// define("ROOT_DIR", $_SERVER["DOCUMENT_ROOT"]."/../");
-// define("TEMPLATES_DIR", ROOT_DIR."views/");
-// /* --- namespaces */
-// define("DEV_NAMESPACE", "fadeev\\php2\\");
-// define("CONTROLLERS_NAMESPACE", DEV_NAMESPACE."controllers\\");
-// /* --- database --- */
-// require_once(ROOT_DIR."config/db.php");
 ?>
